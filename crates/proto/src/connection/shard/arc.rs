7385802c0dec40643ef524691b61924df03b052f
OmniacDEV <blueleafmonkey.gaming@gmail.com>
2025-04-24 19:02:40 +1200

    Connection closed & is_closed

--- crates/proto/src/connection/shard/arc.rs
+++ crates/proto/src/connection/shard/arc.rs
@@ -67,4 +67,12 @@ impl<T: ProtoHelper> ConnectionShared<T> {
 
         Ok(())
     }
+    
+    pub async fn close(&self) {
+        self.connection.read().await.close().await;
+    }
+    
+    pub async fn is_closed(&self) -> bool {
+        self.connection.read().await.is_closed().await
+    }
 }
